a353739bd6b2de63a8c49a2dc920f368b7434c76
nestor <neyblue15@hotmail.com>
2020-11-26 20:36:59 -0500

    Prueba Nestor Fabio Gomez Toro

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,16 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.scss']
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.scss"],
 })
 export class MovieListComponent implements OnInit {
-  movies : Movie[];
+  movies: Movie[];
+  year: any;
+  data: boolean = true;
+  ngOnInit() {}
 
-  ngOnInit() {
+  search() {
+    let url = `https://jsonmock.hackerrank.com/api/movies?Year=${this.year}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.data.length == 0) {
+          this.data = false;
+        } else {
+          this.movies = data.data;
+          this.data = true;
+        }
+      })
+      .catch(console.log);
   }
-
 }
 
 export interface Movie {
